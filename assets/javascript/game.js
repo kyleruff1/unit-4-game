7ba7b5dbd584108cef0ffef1cacd6476325ec140
Kyle Ruff <kyleruff@gmail.com>
2019-06-02 11:01:40 -0600

    started js

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
+var deck = new Array();
+
+
+let createTargetScore1 = document.getElementsByClassName('orignumber');
+
+function createTargetScore()
+{    Math.floor((Math.random() * 100) + 1);
+}
+
+
